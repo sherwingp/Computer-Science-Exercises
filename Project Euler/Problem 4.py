5930da005a6e9fc920dcf3d4650d95ce0b938b32
sherwingp <67482908+sherwingp@users.noreply.github.com>
2020-10-30 14:46:54 +0000

    Update Problem 4.py

--- Project Euler/Problem 4.py
+++ Project Euler/Problem 4.py
@@ -9,14 +9,17 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 biggest_palindrome = 0
 
+# Iterates from 999 to 1
 for i in range(999, 1, -1):
     for j in range(999, 1, -1):
         k = i * j
         reverse_k = str(k)[::-1]
 
+        # Reverses and checks if palindrome
         if str(k) == reverse_k:
             palindrome = k
-
+            
+            # Checks if biggest palindrome
             if palindrome > biggest_palindrome:
                 biggest_palindrome = palindrome
 
