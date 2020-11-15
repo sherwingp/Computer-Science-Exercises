099e29f5990d8ff80df8a4d390f6a88d56063f4e
sherwingp <67482908+sherwingp@users.noreply.github.com>
2020-11-15 16:56:33 +0000

    Add files via upload

---
+++ Statistics/median_calculator.py
@@ -0,0 +1,42 @@
+'''
+Calculating the median
+
+Example from 'Doing Math with Python' ch. 3
+Changed to add user input of a list of numbers
+List input code from https://www.geeksforgeeks.org/python-get-a-list-as-input-from-user/
+'''
+
+def calculate_median(numbers):
+    N = len(numbers)
+    numbers.sort()
+
+    # Find the median
+    if N % 2 == 0:
+        m1 = N/2
+        m2 = (N/2) + 1
+
+        # Convert to integer, match position
+        m1 = int(m1) - 1
+        m2 = int(m2) -1
+        median = (numbers[m1] + numbers[m2]) / 2
+
+    else:
+        m = (N+1) / 2
+
+        # Convert to integer, match position
+        m = int(m) - 1
+        median = numbers[m]
+
+    return median
+
+if __name__ == '__main__':
+    lst = []
+    n = int(input('Enter number of elements: '))
+
+    for i in range(0, n):
+        ele = int(input())
+
+        lst.append(ele)
+    median = calculate_median(lst)
+
+    print('The median is {0}'.format(median))
