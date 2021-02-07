2720c3330eaba0cf56600e4d0887974fb7d1a577
sherwingp <67482908+sherwingp@users.noreply.github.com>
2021-02-07 18:21:00 +0000

    Add files via upload

---
+++ freeCodeCamp/Basic Javascript/use-recursion-to-create-a-range-of-numbers.js
@@ -0,0 +1,9 @@
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const rangeArray = rangeOfNumbers(startNum, endNum - 1);
+    rangeArray.push(endNum);
+    return rangeArray;
+  }
+}
