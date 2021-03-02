a83840157ac03ee4c97b7c1e2cc56bb823b6f6a2
sherwingp <67482908+sherwingp@users.noreply.github.com>
2021-03-02 00:34:31 +0000

    Add files via upload

---
+++ freeCodeCamp/Basic Algorithm Scripting/truncate-a-string.js
@@ -0,0 +1,9 @@
+function truncateString(str, num) {
+    if (str.length > num) {
+      str = str.slice(0, num);
+      str += "...";
+    }
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
