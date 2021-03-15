aba9958269ad72d8870b56e4bbbdab5cfc7d8b3c
sherwingp <67482908+sherwingp@users.noreply.github.com>
2021-03-15 19:30:20 +0000

    Add files via upload

---
+++ freeCodeCamp/Functional Programming/implement-map-on-a-prototype.js
@@ -0,0 +1,21 @@
+/* Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). 
+You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.
+*/
+
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
